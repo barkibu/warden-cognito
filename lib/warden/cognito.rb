aa3834e842e18a20ffafead0337d82ca8ce5af42
Juan F. Pérez <761794+jguitar@users.noreply.github.com>
2020-11-05 19:18:54 +0100

    bundle gem command

---
+++ lib/warden/cognito.rb
@@ -0,0 +1,8 @@
+require "warden/cognito/version"
+
+module Warden
+  module Cognito
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/warden/cognito/version.rb
@@ -0,0 +1,5 @@
+module Warden
+  module Cognito
+    VERSION = "0.1.0"
+  end
+end
