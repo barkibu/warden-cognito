3246fda94c08c91b393ce2f5cdf38f5a75910d6a
Léo <leo@barkibu.com>
2020-11-11 09:56:02 +0100

    Fix rubocop  complaining about constant definition in block

--- spec/support/shared_context/fixtures.rb
+++ spec/support/shared_context/fixtures.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
+StubCognitoUser = Struct.new(:username, :user_attributes)
+AttributeType = Struct.new(:name, :value)
+
 shared_context 'fixtures' do
   let(:user_repository) { Fixtures::UserRepo }
   let(:nil_user_repository) { Fixtures::NilUserRepo }
   let(:user) { Fixtures::User.instance }
 
-  StubCognitoUser = Struct.new(:username, :user_attributes)
-  AttributeType = Struct.new(:name, :value)
   let(:local_identifier) { 'kb_uuid' }
   let(:cognito_user) do
     StubCognitoUser.new('jmj@barkibu.com',
