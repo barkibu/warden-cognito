fba2e6eef548de69ee352456b1d3729e0f44004a
Léo <leo@barkibu.com>
2020-11-24 09:20:43 +0100

    Look for identifying attribute in token payload first

--- lib/warden/cognito/token_decoder.rb
+++ lib/warden/cognito/token_decoder.rb
@@ -26,6 +26,16 @@ module Warden
       end
 
       def user_attribute(attribute_name)
+        token_attribute(attribute_name).presence || cognito_user_attribute(attribute_name)
+      end
+
+      private
+
+      def token_attribute(attribute_name)
+        decoded_token.first[attribute_name] if decoded_token.first.key? attribute_name
+      end
+
+      def cognito_user_attribute(attribute_name)
         cognito_user.user_attributes.detect do |attribute|
           attribute.name == attribute_name
         end&.value
